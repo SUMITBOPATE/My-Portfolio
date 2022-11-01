9f5ea89e2418bd47f7ca7c39996044cb261c279c
SUMITBOPATE <87616955+SUMITBOPATE@users.noreply.github.com>
2022-11-01 09:28:30 -0700

    projects added

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './Project.css';
-
+import img from ".images/quiz.png";
+// import img from ".quiz.png";
 export default function Projects() {
   return (
     <>
@@ -9,7 +10,8 @@ export default function Projects() {
         <div className="head-project">Projects</div>
                    <div className="project-item">
                            <div className="calculator">Calculator</div>
-                            <div className="quiz">Quiz-Game</div>
+                            <div className="quiz">Quiz-Game
+                            <img src={img} alt="quiz-img"/></div>
                             <div className="E-commerce">E-commerce</div> 
                      </div>
       </div>
