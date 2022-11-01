a706ea62d921083f422dacf397d0ddce833a029a
SUMITBOPATE <87616955+SUMITBOPATE@users.noreply.github.com>
2022-11-01 09:08:05 -0700

    1 binaries uploaded

--- src/Projects.js
+++ src/Projects.js
@@ -4,7 +4,15 @@ import './Project.css';
 export default function Projects() {
   return (
     <>
-      <div className="project-main"></div>
+      <div className="project-main">
+        
+        <div className="head-project">Projects</div>
+                   <div className="project-item">
+                           <div className="calculator">Calculator</div>
+                            <div className="quiz">Quiz-Game</div>
+                            <div className="E-commerce">E-commerce</div> 
+                     </div>
+      </div>
     </>
   );
 }
