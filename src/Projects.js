ae9a0b667e5b96f4aaa28045ba9c1753ea2bbc9b
SUMITBOPATE <87616955+SUMITBOPATE@users.noreply.github.com>
2023-01-14 06:57:57 -0800

    1 file modified, 1 deleted

--- src/Projects.js
+++ src/Projects.js
@@ -1,19 +1,20 @@
 import * as React from 'react';
 import './Project.css';
-import img from ".images/quiz.png";
+// import img from ".images/quiz.png";
 // import img from ".quiz.png";
 export default function Projects() {
   return (
     <>
       <div className="project-main">
-        
         <div className="head-project">Projects</div>
-                   <div className="project-item">
-                           <div className="calculator">Calculator</div>
-                            <div className="quiz">Quiz-Game
-                            <img src={img} alt="quiz-img"/></div>
-                            <div className="E-commerce">E-commerce</div> 
-                     </div>
+        <div className="project-item">
+          <div className="calculator">Calculator</div>
+          <div className="quiz">
+            Quiz-Game
+            {/* <img src={img} alt="quiz-img" /> */}
+          </div>
+          <div className="E-commerce">E-commerce</div>
+        </div>
       </div>
     </>
   );
